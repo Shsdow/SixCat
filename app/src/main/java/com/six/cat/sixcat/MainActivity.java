01f0d6de8e44990a35a077383e2d89ff718f54ba
liguoying <alinemklee@yeah.net>
2017-12-06 10:46:54 +0800

    test

--- app/src/main/java/com/six/cat/sixcat/MainActivity.java
+++ app/src/main/java/com/six/cat/sixcat/MainActivity.java
@@ -13,5 +13,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         findViewById(R.id.tv_id).setOnClickListener(view -> {
         });
+
     }
 }
