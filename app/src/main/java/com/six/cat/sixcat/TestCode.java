98aed301ff037cdf1a7b1872cb585c1948228095
liguoying <alinemklee@yeah.net>
2017-12-06 11:11:39 +0800

    delet test code

--- app/src/main/java/com/six/cat/sixcat/TestCode.java
+++
@@ -1,9 +0,0 @@
-package com.six.cat.sixcat;
-
-/**
- * @author liguoying
- * @date 2017/12/6.
- */
-
-public class TestCode {
-}
--- app/src/main/java/com/six/cat/sixcat/service/TestO.java
+++
@@ -1,9 +0,0 @@
-package com.six.cat.sixcat.service;
-
-/**
- * @author liguoying
- * @date 2017/12/6.
- */
-
-public class TestO {
-}
