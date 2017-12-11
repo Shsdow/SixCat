73d0287c06fdd3bda7a641c7ab31e08be2472b46
MLlgy <328620988@qq.com>
2017-12-11 11:13:15 +0800

    basic view

--- app/src/main/java/com/six/cat/sixcat/activity/MainActivity.java
+++ app/src/main/java/com/six/cat/sixcat/activity/MainActivity.java
@@ -1,4 +1,4 @@
-package com.six.cat.sixcat;
+package com.six.cat.sixcat.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -6,14 +6,15 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Toast;
 
+import com.six.cat.sixcat.R;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        findViewById(R.id.tv_id).setOnClickListener(view ->
-                Toast.makeText(MainActivity.this, "", Toast.LENGTH_SHORT).show());
+
     }
 
 
