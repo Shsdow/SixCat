34ba1e684d6295f8c731c19c651a9242a383edab
MLlgy <328620988@qq.com>
2018-02-01 13:56:29 +0800

    界面填充基本数据

---
+++ app/src/main/java/com/six/cat/sixcat/adapter/MovieShowcaseDiscussAdapter.kt
@@ -0,0 +1,9 @@
+package com.six.cat.sixcat.adapter
+
+/**
+ * @author liguoying
+ * @date 2018/2/1.
+ */
+class MovieShowcaseDiscussAdapter {
+
+}
--- app/src/main/java/com/six/cat/sixcat/module/movieshowcase/MovieShowcaseActivity.kt
+++ app/src/main/java/com/six/cat/sixcat/module/movieshowcase/MovieShowcaseActivity.kt
@@ -2,10 +2,14 @@ package com.six.cat.sixcat.module.movieshowcase
 
 import android.os.Bundle
 import android.util.Log
+import com.bumptech.glide.Glide
 import com.six.cat.sixcat.R
 import com.six.cat.sixcat.base.BaseActivity
 import com.six.cat.sixcat.module.movieshowcase.IMovieShowcaseManager.IMoviewShowcasePresenter
+import com.six.cat.sixcat.utils.LogUtil
 import kotlinx.android.synthetic.main.movie_showcase_image.*
+import kotlinx.android.synthetic.main.movie_showcase_movie_content.*
+import java.util.*
 
 /**
  * @author liguoying
@@ -56,6 +60,16 @@ class MovieShowcaseActivity : BaseActivity<IMoviewShowcasePresenter>(), IMovieSh
     }
 
     override fun doSetData(movieShowcaseBean: MovieShowcaseBean) {
+        tvMovieName.text = movieShowcaseBean.original_title
+        var yaear: String? = movieShowcaseBean.year
+        movieShowcaseBean.genres!!.forEach {
+            yaear += "/" + it
+        }
+        tvMovieShortShow.text = String.format(Locale.CHINA, resources.getString(R.string.movie_show_case), yaear, movieShowcaseBean.mainland_pubdate, movieShowcaseBean.durations?.get(0).toString())
+        Glide.with(this).load(movieShowcaseBean.images?.large).into(ivMovieImage)
+        tvMovieScore.text = movieShowcaseBean.rating?.average.toString()
+        tvWatherNum.text = String.format(Locale.CHINA, resources.getString(R.string.movie_show_case_count), movieShowcaseBean.ratings_count)
+        rbStarNum.rating = movieShowcaseBean.rating?.average!!.toFloat() / 2
 
     }
 
