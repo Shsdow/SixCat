2aa38412fbce0583ed80aaf7bbed95f9e84ea426
liguoying <15510224541@163com>
2019-05-17 10:37:25 +0800

    add x

--- app/src/main/java/com/six/cat/sixcat/adapter/HomeFgAdapter2.kt
+++ app/src/main/java/com/six/cat/sixcat/adapter/HomeFgAdapter2.kt
@@ -23,25 +23,17 @@ class HomeFgAdapter2(fm: FragmentManager) : FragmentStatePagerAdapter(fm) {
         )
     }
 
-//    init {
-//        mFragmentList = listOf(
-//                HomeLiveFragment.newInstance(),
-//                HomeRecommendedFragment.newInstance(),
-//                VideoFragment.newInstance(),
-//                HomeRegionFragment.newInstance()
-//        )
-//    }
-
     override fun getItem(position: Int): Fragment {
         return when (position) {
-            0 -> mFragmentList.get(0)
-            1 -> mFragmentList.get(1)
-            2 -> mFragmentList.get(2)
-            3 -> mFragmentList.get(3)
+            0 -> mFragmentList[0]
+            1 -> mFragmentList[1]
+            2 -> mFragmentList[2]
+            3 -> mFragmentList[3]
             else -> mFragmentList[0]
 
         }
     }
 
     override fun getCount() = mFragmentList.size
+
 }
